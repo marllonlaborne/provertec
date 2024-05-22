6d7f20dc1477fc1d1a109d175180c48f168194d8
marllonlaborne <marllonlaborne@gmail.com>
2024-05-21 23:14:21 -0300

    projects carousel slider instanciated and initiated

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,6 @@ import Slides from "./modules/slides.js"
 
 const slides = new Slides('#servicos .carousel-item', '#servicos [data-js=prev-button]', '#servicos [data-js=next-button]')
 slides.init()
+
+const projectsSlides = new Slides('#projetos .projects-info[data-js=carousel-item]', '#projetos [data-js=prev-button]', '#projetos [data-js=next-button]', '#projetos .projects-preview[data-js=carousel-item]', '#projetos .projects-info[data-js=carousel-item]')
+projectsSlides.init()
