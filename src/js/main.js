8305d52c415f0a84415f7a73b71db02f903723c6
marllonlaborne <marllonlaborne@gmail.com>
2024-05-27 23:07:58 -0300

    added a typewriter import and arguments changed from single quote to double quote

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-import Slides from "./modules/slides.js"
+import Slides from './modules/slides.js'
+import Typewriter from './modules/typewriter.js'
 
 const slides = new Slides('#servicos .carousel-item', '#servicos [data-js=prev-button]', '#servicos [data-js=next-button]')
 slides.init()
